52918db2aed170ccfe47fcb9a3b024263b8cb5f8
Micah <mbro36@gmail.com>
2021-06-04 11:37:39 -0600

    created sources_shows seeds

--- src/db/seeds/03_sources_shows.js
+++ src/db/seeds/03_sources_shows.js
@@ -1,10 +1,76 @@
 exports.seed = function (knex) {
     return knex("sources_shows").insert([
         {
-            source_id: "",
-            show_id: "",
+            source_id: "1",
+            show_id: "1",
             full_series: true,
-            show_url: "",
-        }
+            show_url: "https://github.com/micah-patrick",
+        },
+        {
+            source_id: "1",
+            show_id: "2",
+            full_series: true,
+            show_url: "https://github.com/micah-patrick",
+        },
+        {
+            source_id: "2",
+            show_id: "2",
+            full_series: false,
+            show_url: "https://github.com/micah-patrick",
+        },
+        {
+            source_id: "2",
+            show_id: "3",
+            full_series: false,
+            show_url: "https://github.com/micah-patrick",
+        },
+        {
+            source_id: "2",
+            show_id: "1",
+            full_series: true,
+            show_url: "https://github.com/micah-patrick",
+        },
+        {
+            source_id: "3",
+            show_id: "4",
+            full_series: true,
+            show_url: "https://github.com/micah-patrick",
+        },
+        {
+            source_id: "4",
+            show_id: "5",
+            full_series: true,
+            show_url: "https://github.com/micah-patrick",
+        },
+        {
+            source_id: "1",
+            show_id: "6",
+            full_series: true,
+            show_url: "https://github.com/micah-patrick",
+        },
+        {
+            source_id: "2",
+            show_id: "4",
+            full_series: true,
+            show_url: "https://github.com/micah-patrick",
+        },
+        {
+            source_id: "3",
+            show_id: "5",
+            full_series: true,
+            show_url: "https://github.com/micah-patrick",
+        },
+        {
+            source_id: "4",
+            show_id: "6",
+            full_series: false,
+            show_url: "https://github.com/micah-patrick",
+        },
+        {
+            source_id: "3",
+            show_id: "1",
+            full_series: true,
+            show_url: "https://github.com/micah-patrick",
+        },
     ]);
 };
