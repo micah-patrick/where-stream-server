cc22d73489ab861697defa02c022bba70a9698dc
Micah <mbro36@gmail.com>
2021-06-08 09:50:41 -0600

    reduce sources into array inside show list

--- src/shows/shows.service.js
+++ src/shows/shows.service.js
@@ -1,9 +1,19 @@
 const knex = require('../db/connection');
+const reduceProperties = require('../utils/reduce-properties');
 
+const reduceSources = reduceProperties("show_id", {
+    source_id: ["sources", null, "source_id"],
+    source_img_url: ["sources", null, "source_img_url"],
+    show_url: ["sources", null, "show_url"],
+    full_series: ["sources", null, "full_series"],
+})
 
 async function list(){
-    return knex("shows")
-        .select("*");
+    return knex("shows as sh")
+        .join("sources_shows as ss", "sh.show_id", "ss.show_id")
+        .join("sources as so", "ss.source_id", "so.source_id")
+        .select("*")
+        .then((result) => reduceSources(result));
 }
 
 module.exports = {
